770da0c61a1d432e42ad6d05474d81e80f640428
Yuchan <grigomo10@gmail.com>
2026-03-27 20:54:06 +0900

    recyclerview

--- app/src/main/java/com/example/lottery/SearchLotteryNum.kt
+++ app/src/main/java/com/example/lottery/SearchLotteryNum.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lottery.databinding.ActivityMainBinding
 import com.example.lottery.databinding.ActivitySearchBinding
 
@@ -12,7 +14,11 @@ class SearchLotteryNum : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivitySearchBinding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val data = listOf("사과", "배", "포도", "수박")
 
+        recyclerView.layoutManager = LinearLayoutManager(this) // 세로 리스트형
+        recyclerView.adapter = RecyclerViewAdapter(data)
         val btn_back = binding.back
 
         btn_back.setOnClickListener {
